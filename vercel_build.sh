57a0a76092a85c417ecba4c27520fcb1cb6b5dfa
Rodolfo Valdez Castellanos <81203494+rovac24@users.noreply.github.com>
2026-01-19 20:06:54 -0600

    Fix Git safe directory issue and update Flutter config
    Added Git safe directory configuration and adjusted Flutter commands.

--- vercel_build.sh
+++ vercel_build.sh
@@ -8,8 +8,13 @@ FLUTTER_VERSION="3.19.6"
 curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz \
   | tar xJ
 
+# Fix Git safe directory issue (THIS IS THE KEY)
+git config --global --add safe.directory /vercel/path0/flutter
+
 export PATH="$PWD/flutter/bin:$PATH"
 
-flutter doctor
+flutter config --no-enable-linux-desktop
+flutter doctor -v
+
 flutter pub get
 flutter build web --release
