16a5235601cf07b12a06b6cbbc679477766fc8b2
rovac <rovac24@gmail.com>
2026-02-26 18:36:17 -0600

    comma for Miguel

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _OrderLoggerPageState extends State<OrderLoggerPage> {
   'Juan Bayer',
   'Laura Martinez',
   'Maria Camila Gonzalez Montenegro',
-  'Melody Choc'
+  'Melody Choc',
   'Miguel Barreto Diaz',
   'Ognjen Petrovic',
   'Paola Castañon',
